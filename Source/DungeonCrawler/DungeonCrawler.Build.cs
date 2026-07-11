49665b4b851648a749afa151e9bc3d3ad438c2f9
Maciej <raniszem@gmail.com>
2026-07-11 19:45:45 +0200

    Clean up PublicIncludePaths by removing unused directories

--- Source/DungeonCrawler/DungeonCrawler.Build.cs
+++ Source/DungeonCrawler/DungeonCrawler.Build.cs
@@ -24,20 +24,7 @@ public class DungeonCrawler : ModuleRules
 		PrivateDependencyModuleNames.AddRange(new string[] { });
 
 		PublicIncludePaths.AddRange(new string[] {
-			"DungeonCrawler",
-			"DungeonCrawler/Variant_Platforming",
-			"DungeonCrawler/Variant_Platforming/Animation",
-			"DungeonCrawler/Variant_Combat",
-			"DungeonCrawler/Variant_Combat/AI",
-			"DungeonCrawler/Variant_Combat/Animation",
-			"DungeonCrawler/Variant_Combat/Gameplay",
-			"DungeonCrawler/Variant_Combat/Interfaces",
-			"DungeonCrawler/Variant_Combat/UI",
-			"DungeonCrawler/Variant_SideScrolling",
-			"DungeonCrawler/Variant_SideScrolling/AI",
-			"DungeonCrawler/Variant_SideScrolling/Gameplay",
-			"DungeonCrawler/Variant_SideScrolling/Interfaces",
-			"DungeonCrawler/Variant_SideScrolling/UI"
+			"DungeonCrawler"
 		});
 
 		// Uncomment if you are using Slate UI
